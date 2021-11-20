44fc81d006c6a9a2776942cf44d9be30d4c44263
Weiming Wu <weimingw421@gmail.com>
2021-11-19 22:13:17 -0800

    Working for most cases, but not from 09 to 10 or vice versa

--- src/components/PageCounter.js
+++ src/components/PageCounter.js
@@ -1,27 +1,22 @@
 import { useEffect, useState } from 'react';
+import { easeInOutCubic } from 'js-easing-functions';
 import './PageCounter.scss';
 export default function PageCounter(props) {
     const [start, setStart] = useState(1);
     const [target, setTarget] = useState(1);
-    const [moving, setMoving] = useState(false);
 
     useEffect(() => {
-        if (!moving) {
-            setStart(target);
-            setMoving(true);
-        }
+        setStart(target);
         setTarget(props.value);
     }, [props.value]);
 
     function getDirection() {
-        if (moving) {
-            if (target > start) {
-                return 1;
-            } else if (start > target) {
-                return -1;
-            } else {
-                return 0;
-            }
+        if (target > start) {
+            return 1;
+        } else if (start > target) {
+            return -1;
+        } else {
+            return 1;
         }
     }
 
@@ -41,27 +36,86 @@ export default function PageCounter(props) {
     );
 }
 
+const DURATION = 1000;
 const NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 function RollingNumber(props) {
     const { direction, target } = props;
+    const [currentPosition, setCurrentPosition] = useState(getNewPosition());
+    // const [easing, setEasing] = useState(null);
+
+    function getStartPosition() {
+        if (direction < 0) {
+            return currentPosition > -20
+                ? currentPosition - 20
+                : currentPosition;
+        } else {
+            return currentPosition < -20
+                ? currentPosition + 20
+                : currentPosition;
+        }
+    }
+
+    function getMovement(startPosition) {
+        if (direction < 0) { // value going down
+            if (-2 * target - 20 > startPosition) {
+                return -2 * target - 20 - startPosition;
+            }
+            return -2 * target - startPosition;
+        } else { // value going up
+            if (-2 * target > startPosition) {
+                return -2 * target - 20 - startPosition;
+            }
+            return -2 * target - startPosition;
+        }
+    }
+
+    useEffect(() => {
+        const startTime = new Date();
+        const startPosition = getStartPosition();
+        const movement = getMovement(startPosition);
+
+        // do adjustment here
+
+        function tick() {
+            const elapsed = new Date() - startTime;
+            const position = easeInOutCubic(
+                elapsed,
+                startPosition,
+                movement,
+                DURATION
+            );
+            setCurrentPosition(position);
+
+            if (elapsed < DURATION) {
+                requestAnimationFrame(tick);
+            }
+        }
+
+        tick();
+    }, [target]);
 
     function renderNumbers() {
         return NUMS.map((n, i) => <span key={i}>{n}</span>);
     }
 
     function getNewPosition() {
-        if (direction > 0) {
-            return `${target * -2}em`;
-        } else {
-            return `${target * 2}em`;
-        }
+        return target * -2;
     }
 
     return (
         <div class="rolling-number-window">
             <div class="rolling-number-placeholder"></div>
-            <div class="rolling-number" style={{ transform: `translateY(${getNewPosition()})` }}>{renderNumbers()}</div>
-            <div style={{ transform: 'translateX(20em)' }}>{target}</div>
+            <div
+                class="rolling-number"
+                style={{ transform: `translateY(${roundNumber(currentPosition, 3)}em)` }}
+            >
+                {renderNumbers()}
+            </div>
         </div>
     );
+}
+
+function roundNumber(num, digits) {
+    const decimal = Math.pow(10, digits)
+    return Math.round(num * decimal) / decimal;
 }
