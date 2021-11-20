137ed0ed60678be5aab2084f4d9597711d19a84d
Weiming Wu <weimingw421@gmail.com>
2021-11-19 20:25:59 -0800

    Setup done

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
+import { useState } from 'react';
+import PageCounter from './components/PageCounter';
 
-const App = () => <div>
-    <h1>H1 text</h1>
-    <p>Lorem ipsum dolor</p>
-    <p>Lorem ipsum dolor</p>
-    <h2>H2 text</h2>
-    <p>Lorem ipsum dolor</p>
-    <p>Lorem ipsum dolor</p>
-    <h3>H3 text</h3>
-    <p>Lorem ipsum dolor</p>
-    <p>Lorem ipsum dolor</p>
-    <h4>H4 text</h4>
-    <p>Lorem ipsum dolor</p>
-    <p>Lorem ipsum dolor</p>
-    <h5>H5 text</h5>
-    <p>Lorem ipsum dolor</p>
-    <p>Lorem ipsum dolor</p>
-    <h6>H6 text</h6>
-    <p>Lorem ipsum dolor</p>
-    <p>Lorem ipsum dolor</p>
+const App = () => {
+    const [value, setValue] = useState(0);
+    const [counterValue, setCounterValue] = useState(0);
 
-    <button>Some text</button>
-    <input type='button' value='Some text' />
-    <label>
-        <input type='checkbox' />
-        <span>Some text</span>
-    </label>
-    <a href='https://www.google.com'>Standard link</a>
-</div>;
+    function applyValue() {
+        setCounterValue(value);
+    }
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                type="number"
+            />
+            <button onClick={applyValue}>Click to Change</button>
+            <PageCounter value={counterValue} />
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/PageCounter.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import './PageCounter.scss';
+export default function PageCounter(props) {
+    const [start, setStart] = useState(1);
+    const [target, setTarget] = useState(1);
+    const [moving, setMoving] = useState(false);
+
+    useEffect(() => {
+        if (!moving) {
+            setStart(target);
+            setMoving(true);
+        }
+        setTarget(props.value);
+    }, [props.value]);
+
+    function getDirection() {
+        if (moving) {
+            if (target > start) {
+                return 1;
+            } else if (start > target) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    return (
+        <div className="page-counter">
+            <div className="page-counter-numerator">
+                <RollingNumber
+                    direction={getDirection()}
+                    target={Math.floor(target / 10)}
+                />
+                <RollingNumber
+                    direction={getDirection()}
+                    target={target % 10}
+                />
+            </div>
+        </div>
+    );
+}
+
+const NUMS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+function RollingNumber(props) {
+    const { direction, target } = props;
+
+    function renderNumbers() {
+        return NUMS.map((n, i) => <span key={i}>{n}</span>);
+    }
+
+    return (
+        <div class="rolling-number-window">
+            <div class="rolling-number-placeholder"></div>
+            <div class="rolling-number">{renderNumbers()}</div>
+        </div>
+    );
+}
